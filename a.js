b86bcc2e9463516a23210048153ba6131973a4be
YuvRaj6392 <74025229+YuvRaj6392@users.noreply.github.com>
2024-04-05 17:31:18 +0530

    added

---
+++ a.js
@@ -0,0 +1,5 @@
+"use strict";
+function greet(firstName, lastName, age) {
+    console.log(firstName + lastName + age);
+}
+greet("yuvraj", "srivastava", 24);
---
+++ a.ts
@@ -0,0 +1,5 @@
+function greet(firstName:string,lastName:string,age:number){
+ console.log(firstName+lastName+age)
+}
+
+greet("yuvraj","srivastava",24)
